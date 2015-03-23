338b9d367523f40610a3049aaea8dd12ab5dbca6
Hector Jose Flores Colmenarez <hecto932@gmail.com>
2015-03-22 21:52:12 -0430

    Agregando contador de usuarios y mensajes con nombres

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var usersConnected = 0;
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-	console.log('A user connected!');
+	usersConnected++;
+	console.log('Usuarios conectados: ' + usersConnected);
+	socket.on('chat message', function(msg){
+		console.log('message --> ' + msg);
+		io.emit('chat message', msg);
+	});
+	socket.on('disconnect', function(){
+	    console.log('user disconnected');
+	    usersConnected--;
+	});
 });
 
-http.listen(3000, function(){
+http.listen(1337, function(){
 	console.log('listening on http://localhost:3000/');
 });
